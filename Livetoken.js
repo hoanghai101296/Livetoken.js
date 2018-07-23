e03df4530fb56d0d497f98e925994e37da7a21fe
hoanghai101296 <36188322+hoanghai101296@users.noreply.github.com>
2018-07-24 00:04:02 +0700

    Update and rename particles.js to Livetoken.js

--- Livetoken.js
+++ Livetoken.js
@@ -1,8 +1,8 @@
 /* -----------------------------------------------
 /* Author : Vincent Garreau  - vincentgarreau.com
 /* MIT license: http://opensource.org/licenses/MIT
-/* Demo / Generator : vincentgarreau.com/particles.js
-/* GitHub : github.com/VincentGarreau/particles.js
+/* Demo / Generator : https://github.com/hoanghai101296/Livetoken.js
+/* GitHub : https://github.com/hoanghai101296/Livetoken.js
 /* How to use? : Check the GitHub README
 /* v2.0.0
 /* ----------------------------------------------- */
